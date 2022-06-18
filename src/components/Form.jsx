dc1df3e4d136ae01cf04eb864ca6860313ba36a9
Navaneet1999 <93372592+Navaneet1999@users.noreply.github.com>
2022-06-19 00:55:54 +0530

    second

---
+++ src/components/Form.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react"
+import axios from "axios";
+
+export const Form = () => {
+    const url = "https://boiling-lowlands-89127.herokuapp.com/"
+    const [data, setData] = useState({
+        name: "",
+        department: "",
+        email: "",
+        mobile: "",
+        address: ""
+    });
+    async function submit(e) {
+        // e.preventDefault();
+        await fetch(url, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+          });
+        // axios.post(url,{
+        //     name: data.name,
+        //     department: data.department,
+        //     email: data.email,
+        //     mobile: data.mobile,
+        //     address: data.address
+        // })
+        // .then(res=>{
+        //     console.log(res.data);
+        // })
+    }
+
+    function handle(e) {
+        const newdata = {...data}
+        newdata[e.target.id] = e.target.value
+        setData(newdata)
+        console.log(newdata)
+    }
+
+    return (
+        <div>
+            <h1>STUDENT ENTRY FORM</h1>
+            <form onSubmit={(e) => submit(e)}>
+                <input type="text" value={data.name} id="name" placeholder="Enter your name" onChange={(e) => handle(e)}/>
+                <input type="text" value={data.department} id="department" placeholder="Enter your department" onChange={(e) => handle(e)}/>
+                <input type="text" value={data.email} id="email" placeholder="Enter your email" onChange={(e) => handle(e)}/>
+                <input type="text" value={data.mobile} id="mobile" placeholder="Enter your mobile" onChange={(e) => handle(e)}/>
+                <input type="text" value={data.address} id="address" placeholder="Enter your address" onChange={(e) => handle(e)}/>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
---
+++ src/components/List.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react"
+import "./List.css"
+
+export const List = () => {
+    const [datas, setDatas] = useState([]);
+    useEffect(() => {
+        fetch("https://boiling-lowlands-89127.herokuapp.com/")
+        .then(response => response.json())
+            // 4. Setting *dogImage* to the image url that we received from the response above
+        .then(data => setDatas(data))
+    },[]);
+    console.log("data", datas)
+
+    return (
+        <div className="App">
+            <h1>STUDENT LIST</h1>
+            <table >
+                <tr>
+                    <td>NAME</td>
+                    <td>DEPARTMENT</td>
+                    <td>EMAIL</td>
+                    <td>MOBILE</td>
+                    <td>ADDRESS</td>
+                </tr>
+                {datas.map((data)=>(
+                    <tr key={data.id}>
+                        <td>{data.name}</td>
+                        <td>{data.department}</td>
+                        <td>{data.email}</td>
+                        <td>{data.mobile}</td>
+                        <td>{data.address}</td>
+                    </tr>
+                ))}
+            </table>
+        </div>
+    )
+}
